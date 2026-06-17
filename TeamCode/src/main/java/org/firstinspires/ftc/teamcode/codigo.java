e14d0beec934a1e85ddce94926b7f2c195c14086
joaquim_mm_silva <joaquim_mm_silva@estudante.sesisenai.org.br>
2026-06-17 17:22:18 -0300

    robo andar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codigo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codigo.java
@@ -4,17 +4,26 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "teste")
+@TeleOp(name = "teste2")
 
-public class codigo extends OpMode {
+public class codigo
+        extends OpMode {
 
 
     private DcMotor intake;
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
 
     @Override
     public void init() {
 
         intake = hardwareMap.get(DcMotor.class, "intake");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
     }
 
     @Override
@@ -27,6 +36,14 @@ public class codigo extends OpMode {
         } else {
             intake.setPower(0);
         }
+
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+        frontLeftMotor.setPower(y + x + rx);
+        backLeftMotor.setPower(y - x + rx);
+        frontRightMotor.setPower(y - x - rx);
+        backRightMotor.setPower(y + x - rx);
 
     }
 }
